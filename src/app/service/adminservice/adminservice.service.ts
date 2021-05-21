e552b5d9ce5c725bbf12ac8e83a226353ee5e339
omkarmali4437 <omkarmalli688@gmail.com>
2021-05-22 01:02:39 +0530

    Added methods for api calls for admin

--- src/app/service/adminservice/adminservice.service.ts
+++ src/app/service/adminservice/adminservice.service.ts
@@ -1,3 +1,4 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpServiceService } from '../httpservie/http-service.service'
 
@@ -8,8 +9,29 @@ export class AdminserviceService {
 
   constructor(private http : HttpServiceService) { }
 
-  login(data:any){
-    return this.http.post('admin/login',data)
+  accessToken = localStorage.getItem('access');
+
+  options = {
+    headers: new HttpHeaders({
+      'x-access-token': this.accessToken,
+      'Content-Type': 'application/json',
+      'accept': 'application/json'
+    })
   }
 
+  login(data:any){
+    return this.http.post('admin/login',data,'')
+  }
+
+  addBook(data : any){
+    return this.http.post('admin/add/book',data,this.options);
+  }
+
+  getBooks(){
+    return this.http.get('get/book',this.options);
+  }
+
+  deleteBook(id : any){
+    return this.http.delete('admin/delete/book/'+id,this.options)
+  }
 }
