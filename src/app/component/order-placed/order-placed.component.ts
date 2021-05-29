328631b0e1e0d95064021b1523cb502e5ece8d83
omkarmali4437 <omkarmalli688@gmail.com>
2021-05-29 10:20:08 +0530

    Added routing to order placed page

--- src/app/component/order-placed/order-placed.component.ts
+++ src/app/component/order-placed/order-placed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-placed',
@@ -7,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderPlacedComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
   }
 
+  route(){
+    this.router.navigate(['/userdashboard'])
+  }
 }
